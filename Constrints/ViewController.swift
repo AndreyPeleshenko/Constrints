8929ef22df7cc40309d38c983645e96936804dff
Andrey <andrey-gezet@mail.ru>
2023-05-16 20:41:25 +0300

    Initial Commit

---
+++ Constrints/ViewController.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var trafficLightRedView: UIView!
+    @IBOutlet var trafficLightYellowView: UIView!
+    @IBOutlet var trafficLightGreenView: UIView!
+    @IBOutlet var trafficLightSwitchButton: UIButton!
+    
+    let colorOfLight: ColorLight = .red
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        trafficLightRedView.layer.cornerRadius = trafficLightRedView.frame.height / 2
+        trafficLightYellowView.layer.cornerRadius = trafficLightYellowView.frame.height / 2
+        trafficLightGreenView.layer.cornerRadius = trafficLightGreenView.frame.height / 2
+    }
+    
+    @IBAction func LightSwith(_ sender: UIButton) {
+        
+    }
+}
+
+extension ViewController {
+    enum ColorLight {
+        case red
+        case yellow
+        case green
+    }
+}
